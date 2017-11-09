533a38eb89c642bec0a319f9f68988929ed86896
christinaswyers <christinaswyers@gmail.com>
2017-11-09 15:39:12 -0600

    added agent focused styles

--- front-page.php
+++ front-page.php
@@ -25,7 +25,7 @@ get_header(); ?>
 				<div class="widget-area">
 					<section id="property-search-6" class="widget property-search">
 						<div class="widget-wrap">
-							<form role="search" method="get" id="searchform" action="https://demo.winningagent.com/agent-focused/">
+							<form role="search" method="get" id="searchform" action="/property-search/">
 
 								<input value="" name="s" type="hidden">
 									<input value="listing" name="post_type" type="hidden">
@@ -181,16 +181,6 @@ get_header(); ?>
 
 	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
 
-<section class="hidden">
-       <link type="text/css" rel="stylesheet" href="http://www.marissearch.com/basic.css" />
-<script type="text/javascript"> document.write('<div id="listing_widget_74379e4db9dfa5385f7f38f7e9463d9a"></div>'); (function() {
-var wc = document.createElement('script');
-wc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.marissearch.com/?id=listing_widget_74379e4db9dfa5385f7f38f7e9463d9a&iden=15091-4d9c70f6d0&AuthKey=e0d7b67499421bf5dd52b3eb0879a1bf&street=1&city=1&county=1&mlsArea=1&mls=1&price=1&bedbaths=1&stypes=1&label=Search&titles=1&rand='+Math.random();
-wc.setAttribute('async','true'); document.documentElement.firstChild.appendChild(wc);
-})();
-</script>
-
-</section>
 
 
 	</main><!-- #main -->
