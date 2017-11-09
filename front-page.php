b89209fc8167a345ccd03229408a2e1f443bfb36
christinaswyers <christinaswyers@gmail.com>
2017-11-09 15:29:13 -0600

    added agent focused styles

--- front-page.php
+++ front-page.php
@@ -150,13 +150,14 @@ get_header(); ?>
 </div></div></div>
 
 		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
+		// if ( have_posts() ) :
+		// 	while ( have_posts() ) : the_post();
+		// 		get_template_part( 'template-parts/page/content', 'front-page' );
+		// 	endwhile;
+		// else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+		// 	get_template_part( 'template-parts/post/content', 'none' );
+		// endif; 
+		?>
 
 		<?php
 		// Get each of our panels and show the post data.
