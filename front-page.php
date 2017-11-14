4a3316fef31a825a4dbec074983c955741cbab5c
christinaswyers <christinaswyers@gmail.com>
2017-11-14 10:49:33 -0600

    Added content to frontpage"

--- front-page.php
+++ front-page.php
@@ -80,10 +80,10 @@ get_header(); ?>
 						// Show the selected frontpage content.
 						if ( have_posts() ) :
 							while ( have_posts() ) : the_post();
-								get_template_part( 'template-parts/page/content', 'none' );
+								// get_template_part( 'template-parts/page/content', 'none' );
 							endwhile;
 						else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-							get_template_part( 'template-parts/post/content', 'none' );
+							// get_template_part( 'template-parts/post/content', 'none' );
 						endif;
 						?>
 						 </div>
