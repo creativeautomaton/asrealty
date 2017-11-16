16a66add80e9b10598a61ca2b1f4dfa2caab91d6
christinaswyers <christinaswyers@gmail.com>
2017-11-16 08:48:25 -0600

    tdays gits

--- front-page.php
+++ front-page.php
@@ -183,8 +183,8 @@ get_header(); ?>
 				<?php
 						$args = array( 'post_type' => 'communities', 'posts_per_page' => 4 );
 						$loop = new WP_Query( $args );
-						 if ( $loop->have_posts() ) : $loop->the_post(); {
-								while ( have_posts() ) {
+						 if ( have_posts($loop) ) : the_post($loop); {
+								while ( have_posts($loop) ) {
 				?>
 				<article class="post-514 wap-community type-wap-community status-publish has-post-thumbnail wap-community-type-family-friendly wap-community-type-parks entry">
 					<a href="https://demo.winningagent.com/agent-focused/community/the-lakelands/" title="The Lakelands" class="wap-community-link">
@@ -209,7 +209,7 @@ get_header(); ?>
 				</div>
 			</article>
 				<?php
-							
+
 							} // end while
 						} // end if
 				?>
